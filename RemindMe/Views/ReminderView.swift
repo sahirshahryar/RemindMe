2a30461d189b7b98871382977d6a95c74531eae9
Sahir Shahryar <sahirshahryar@gmail.com>
2020-02-16 18:03:23 -0500

    Clean up ReminderView code
    1. Added a @State value to the Checkbox view to force SwiftUI to update the graphic whenever the checkbox is pressed
    2. Modified the ReminderView UI to generally match what is shown in the prototype graphic
    3. Added some basic accessbility scaling

--- RemindMe/Views/ReminderView.swift
+++ RemindMe/Views/ReminderView.swift
@@ -11,16 +11,68 @@ import SwiftUI
 
 struct Checkbox: View {
 
+    /**
+     * Actual connection to data model; this allows data to stay in sync with what
+     * is shown on-screen
+     */
     @Binding var enabled: Bool
 
-    private func toggle() {
-        self.enabled.toggle()
+    /**
+     * "Fake" state variable to force SwiftUI to refresh the view when clicked
+     */
+    @State var innerState: Int
+
+    @Environment(\.sizeCategory) var size
+
+    let feedback = UIImpactFeedbackGenerator()
+
+    /**
+     * Scale image to match accessibility size. Doesn't work as of right now
+     */
+    private func scale() -> CGFloat {
+        switch size {
+        case .large:
+            return 1.04
+        case .extraExtraLarge:
+            return 1.08
+        case .extraExtraExtraLarge:
+            return 1.12
+        case .accessibilityMedium:
+            return 1.16
+        case .accessibilityLarge:
+            return 1.20
+        case .accessibilityExtraLarge:
+            return 1.24
+        case .accessibilityExtraExtraLarge:
+            return 1.30
+        case .accessibilityExtraExtraExtraLarge:
+            return 1.38
+        default:
+            return 1
+        }
     }
 
+    /**
+     * Toggles button status and increments `innerState` to force a UI refresh
+     */
+    private func flip() {
+        self.enabled.toggle()
+        self.innerState += 1
+        feedback.impactOccurred()
+    }
+
+    /**
+     * Defines checkbox
+     */
     public var body: some View {
-        Button (action: toggle) {
-            Image(systemName: enabled ? "checkmark.square" : "square")
+        Button (action: flip) {
+            Image(enabled ? "checkbox-on" : "checkbox-off")
+                .renderingMode(.original)
+                // .scaleEffect(scale()) -> TODO: Doesn't do anything right now
         }
+            .frame(width: nil, height: nil, alignment: .topLeading)
+            .padding(.top, 0)
+            .padding([.leading, .trailing], 6)
     }
 
 }
@@ -31,30 +83,106 @@ struct Checkbox: View {
 struct ReminderView: View {
 
     @State var reminder: Reminder
+    @Environment(\.sizeCategory) var size
+
+    /**
+     * Function to determine reminder height based on accessibility size
+     */
+    func width() -> CGFloat {
+        switch size {
+        case .extraSmall:
+            return 200
+        case .small:
+            return 220
+        case .medium:
+            return 240
+        case .large:
+            return 260
+        case .extraLarge:
+            return 280
+        case .extraExtraLarge:
+            return 300
+
+        /**
+         * Right now these size limits are designed around the 375x812 canvas found on
+         * the 5.8" X-class iPhones. We artificially limit it to 320 pts so that it is
+         * always visible whether there is another reminder in the list (the next one
+         * will be barely visible on the right edge of the screen). Any wider and we
+         * would not be able to see those. We can make room for additional content by
+         * making the reminder *taller*, not wider.
+         */
+        default:
+            return 320
+        }
+    }
+
+    /**
+     * Function to determine reminder height based on accessibility size
+     */
+    func height() -> CGFloat {
+        switch size {
+        case .extraSmall:
+            return 80
+        case .small:
+            return 90
+        case .medium:
+            return 100
+        case .large:
+            return 110
+        case .extraLarge:
+            return 120
+        case .extraExtraLarge:
+            return 130
+        case .extraExtraExtraLarge:
+            return 140
+        case .accessibilityMedium:
+            return 160
+        case .accessibilityLarge:
+            return 180
+        case .accessibilityExtraLarge:
+            return 200
+        case .accessibilityExtraExtraLarge:
+            return 220
+        case .accessibilityExtraExtraExtraLarge:
+            return 250
+        @unknown default:
+            return 160
+        }
+    }
 
     public var body: some View {
-        let completeness = Binding<Bool>(
-            get: {
-                return self.reminder.complete
-            },
-
-            set: {
-                self.reminder.complete = $0
-                print("New value for reminder \(self.reminder.title!) = \($0)")
-            }
+        // Binding to connect CoreData object to checkbox
+        let completeness = Binding(
+            get: { self.reminder.complete },
+            set: { self.reminder.complete = $0 }
         )
 
-        return HStack {
-            Checkbox(enabled: completeness)
-            VStack {
+        //
+        return HStack(alignment: .top, spacing: 2) {
+            Checkbox(enabled: completeness, innerState: 0)
+                .padding(.top, 6)
+            VStack(alignment: .leading, spacing: 4) {
                 Text(self.reminder.title ?? "Untitled")
-                Text(self.reminder.desc ?? "No description")
+                    .font(.system(size: 20, design: .rounded))
+                    .fontWeight(.semibold)
+                    .lineLimit(2)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .padding(.top, 3)
+                    .padding(.bottom, -6)
+                Spacer(minLength: 0)
+                Text(self.reminder.desc ?? "No details")
+                    .foregroundColor(Color.init(UIColor.systemGray))
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 2)
+                    .padding(.top, -2)
             }
+            .padding([.top, .bottom, .trailing], 4)
         }
-        .frame(width: 150, height: 80, alignment: .leading)
+        .frame(width: width(), height: height(), alignment: .leading)
         .background(
             RoundedRectangle(cornerRadius: 8)
                 .fill(Color.init(UIColor.systemGray6))
+                .opacity(0.9)
         )
     }
 
