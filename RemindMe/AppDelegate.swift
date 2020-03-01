0628bc19b428b6f5ce1df044a1f89af15aede8b6
Sahir Shahryar <sahirshahryar@gmail.com>
2020-03-01 15:43:35 -0500

    Add licensing / documentation headers

--- RemindMe/AppDelegate.swift
+++ RemindMe/AppDelegate.swift
@@ -1,17 +1,46 @@
-//
-//  AppDelegate.swift
-//  RemindMe
-//
-//  Created by Sahir Shahryar on 1/16/20.
-//  Copyright © 2020 Sahir Shahryar. All rights reserved.
-//
+/*
+ * This file is part of RemindMe, licensed under the MIT License (MIT).
+ *
+ * Copyright © Sahir Shahryar <https://github.com/sahirshahryar/>
+ * This program is built as part of a personal portfolio, and will not necessarily
+ * be maintained by its author.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
+/**
+ * `AppDelegate.swift` | `RemindMe`
+ *
+ * - Authors: Sahir Shahryar <https://github.com/sahirshahryar/>
+ * - Since: Thursday, January 16, 2020
+ * - Version: 1.0.0
+ */
 import UIKit
 import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    /**
+     * Fetcher for the `NSPersistentContainer` that we use to store reminder data,
+     * schedules, categories, et cetera.
+     */
     var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "RemindersModel")
         container.loadPersistentStores(completionHandler: { desc, errors in
@@ -24,10 +53,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return container
     }()
 
+
+    /**
+     * Gets the currently active `AppDelegate` singleton.
+     */
+    static func get() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
+
+
+    /**
+     * Gets the `NSManagedObjectContext` of the persistence model for reads / writes.
+     */
     static func getContext() -> NSManagedObjectContext {
         return AppDelegate.get().persistentContainer.viewContext
     }
 
+
+    /**
+     * Attempts to save any changes to the data model to disk.
+     *
+     * - Returns: `(Bool)` `true` if the save was successful or if no changes have been
+     *            made since the last write; `false` if and only if there was an error
+     *            while trying to save changes.
+     */
     static func saveContext() -> Bool {
         let context = getContext()
         if context.hasChanges {
@@ -43,29 +92,43 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+
+    /**
+     *
+     */
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)
+        -> Bool {
+            // Override point for customization after application launch.
+            return true
     }
+
 
     // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    /**
+     *
+     */
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        return UISceneConfiguration(name: "Default Configuration",
+                                    sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    /**
+     *
+     */
+    func application(_ application: UIApplication,
+                     didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        // If any sessions were discarded while the application was not running,
+        // this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded
+        // scenes, as they will not return.
     }
-
-    static func get() -> AppDelegate {
-        return UIApplication.shared.delegate as! AppDelegate
-    }
-
 
 }
 
--- RemindMe/SceneDelegate.swift
+++ RemindMe/SceneDelegate.swift
@@ -1,19 +1,53 @@
-//
-//  SceneDelegate.swift
-//  RemindMe
-//
-//  Created by Sahir Shahryar on 1/16/20.
-//  Copyright © 2020 Sahir Shahryar. All rights reserved.
-//
+/*
+ * This file is part of RemindMe, licensed under the MIT License (MIT).
+ *
+ * Copyright © Sahir Shahryar <https://github.com/sahirshahryar/>
+ * This program is built as part of a personal portfolio, and will not necessarily
+ * be maintained by its author.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
 
+/**
+ * `SceneDelegate.swift` | `RemindMe`
+ *
+ * - Authors: Sahir Shahryar <https://github.com/sahirshahryar/>
+ * - Since: Thursday, January 16, 2020
+ * - Version: 1.0.0
+ */
 import UIKit
 import SwiftUI
 import CoreData
 
+/**
+ *
+ */
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    /**
+     *
+     */
     var window: UIWindow?
 
+    /**
+     *
+     */
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -25,7 +59,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         let context = AppDelegate.getContext()
 
         /**
-         * Temporary code
+         * Temporary code to add CoreData objects. This will no longer be necessary
+         * once there are UI elements available to actually add items dynamically.
          */
         var category: Category? = nil
         for value in ["Alpha", "Beta", "Gamma"] {
@@ -40,7 +75,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             print("Error saving")
         }
 
-        let contentView = ContentView(category: category!).environment(\.managedObjectContext, context)
+        let contentView = ContentView(category: category!)
+            .environment(\.managedObjectContext, context)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
@@ -51,6 +87,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
     }
 
+
+    /**
+     *
+     */
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -58,27 +98,42 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
     }
 
+
+    /**
+     *
+     */
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
 
+
+    /**
+     *
+     */
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
 
+
+    /**
+     *
+     */
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
 
+
+    /**
+     *
+     */
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
 
 }
 
